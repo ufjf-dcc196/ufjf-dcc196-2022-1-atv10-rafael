369049c78dcdede18d408e26e4255cad6fd0d153
Rafel Freesz <rafaelfreesz@gmail.com>
2022-08-05 06:00:41 -0300

    Criada a Activity de Novo Jogo

---
+++ app/src/main/java/br/ufjf/dcc196/rafael/agente008/NovoJogoActiviry.java
@@ -0,0 +1,14 @@
+package br.ufjf.dcc196.rafael.agente008;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NovoJogoActiviry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_novo_jogo_activiry);
+    }
+}
