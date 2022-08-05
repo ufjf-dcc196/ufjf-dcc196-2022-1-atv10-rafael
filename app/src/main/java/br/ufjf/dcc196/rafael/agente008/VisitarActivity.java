f8a1dd3ae399d90b500fc3e78c7d77fe8bcecd5b
Rafel Freesz <rafaelfreesz@gmail.com>
2022-08-05 06:39:25 -0300

    Criada a Activity Visitar

---
+++ app/src/main/java/br/ufjf/dcc196/rafael/agente008/VisitarActivity.java
@@ -0,0 +1,14 @@
+package br.ufjf.dcc196.rafael.agente008;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VisitarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_visitar);
+    }
+}
