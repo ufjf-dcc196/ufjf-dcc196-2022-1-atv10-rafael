a57c64e0dc75ce0465decff4f64abb04a73778e6
Rafel Freesz <rafaelfreesz@gmail.com>
2022-08-05 06:48:27 -0300

    Criada a Activity Viajar

--- app/src/main/java/br/ufjf/dcc196/rafael/agente008/ViajarActivity.java
+++ app/src/main/java/br/ufjf/dcc196/rafael/agente008/ViajarActivity.java
@@ -1,14 +1,14 @@
 package br.ufjf.dcc196.rafael.agente008;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class NovoJogoActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ViajarActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_novo_jogo_activity);
+        setContentView(R.layout.activity_visitar);
     }
 }
